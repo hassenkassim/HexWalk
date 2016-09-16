4735d9401c98cc7ef19463bd8368388ed3d30367
Hassen Kassim <hass3n@googlemail.com>
2016-09-16 19:22:53 +0200

    Feldgröße angepasst

--- Assets/Script/LevelStart.cs
+++ Assets/Script/LevelStart.cs
@@ -5,7 +5,7 @@ public class LevelStart : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Gamefield gamefield = new Gamefield (5, 5);
+		Gamefield gamefield = new Gamefield (10, 5);
 		gamefield.getField (1, 1).changeColor (Color.cyan);
 	}
 	

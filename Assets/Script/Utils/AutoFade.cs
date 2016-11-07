f143a0b99b0d7db6ad1007922eb666fbbb921cb4
Dursun Gök <dursun38@hotmail.de>
2016-11-07 22:20:03 +0100

    Starfield-> BackgroundManager; Changes in Fading;

--- Assets/Script/Utils/AutoFade.cs
+++ Assets/Script/Utils/AutoFade.cs
@@ -4,45 +4,35 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class AutoFade{
-
+	
 	//public static GameObject tmp;
-	public static float fadeTime=0.5f; 
+	public static float fadeTime=1.0f; 
 	public static PrefabsManagerLevelPlay prefabsMgr;
 	public  GUITexture overlay;
 	public bool fadeIn;
+	public bool fadeOut;
+	public bool fadeInOut;
 
+	public bool fadeReady=false;
 
 
 	public AutoFade(GameObject overlay1)
 	{
+
 		this.fadeIn = false;
+		this.fadeOut = false;
+		this.fadeInOut = false;
 		this.overlay=overlay1.GetComponent<GUITexture> ();
 	}
 
-
+	//fade to clear:
 	public IEnumerator FadeIn(){
-		//fade to black:
-		overlay.gameObject.SetActive (true);
-		overlay.color = Color.clear;
-		InputManager.active=false;
-
 		float rate = 1.5f/fadeTime;
 		float progress = 0.0f;
-		while (progress<1.0f) {
-			overlay.color = Color.Lerp (Color.clear, Color.black, progress);
-			progress += rate * Time.deltaTime;
-			yield return null;
-		}
-		overlay.color = Color.black;
-
-		// change the world here ....
-		yield return null;
-		LevelPlay.changeWorld = true;
-
-		//fade to clear:
 		this.overlay.color = Color.black;
 
-		progress = 0.0f;
+		InputManager.active=false;
+
 		while (progress<1.0f) {
 			this.overlay.color = Color.Lerp (Color.black, Color.clear, progress);
 			progress += rate * Time.deltaTime;
@@ -50,9 +40,71 @@ public class AutoFade{
 		}
 		this.overlay.color = Color.clear;
 		this.overlay.gameObject.SetActive (false);
-		LevelPlay.fading.fadeIn = false;
 		InputManager.active=true;
+		fadeReady = true;
+	}
+
+	//fade to black:
+	public IEnumerator FadeOut(){
+		float rate = 1.5f/fadeTime;
+		float progress = 0.0f;
+
+		overlay.color = Color.clear;
+		overlay.gameObject.SetActive (true);
+		InputManager.active=false;
+
+		while (progress<1.0f) {
+			overlay.color = Color.Lerp (Color.clear, Color.black, progress);
+			progress += rate * Time.deltaTime;
+			yield return null;
+		}
+		overlay.color = Color.black;
+		LevelPlay.fading.fadeOut = false;
+		InputManager.active=true;
+		fadeReady = true;
+		overlay.gameObject.SetActive (false);
 
 	}
+
+	public IEnumerator FadeInOut(){
+
+		float rate = 1.5f/fadeTime;
+		float progress = 0.0f;
+
+		overlay.color = Color.clear;
+		overlay.gameObject.SetActive (true);
+		InputManager.active=false;
+
+		while (progress<1.0f) {
+			overlay.color = Color.Lerp (Color.clear, Color.black, progress);
+			progress += rate * Time.deltaTime;
+			yield return null;
+		}
+		overlay.color = Color.black;
+		LevelPlay.fading.fadeOut = false;
+		InputManager.active=true;
+		fadeReady = true;
+
+
+		LevelPlay.changeWorld = true;
+
+		rate = 1.5f/fadeTime;
+		progress = 0.0f;
+		this.overlay.color = Color.black;
+
+		InputManager.active=false;
+
+		while (progress<1.0f) {
+			this.overlay.color = Color.Lerp (Color.black, Color.clear, progress);
+			progress += rate * Time.deltaTime;
+			yield return null;
+		}
+		this.overlay.color = Color.clear;
+		this.overlay.gameObject.SetActive (false);
+		InputManager.active=true;
+		fadeReady = true;
+	}
+
+
 }   
 
--- Assets/Script/Utils/AutoFadeController.cs
+++ Assets/Script/Utils/AutoFadeController.cs
@@ -3,35 +3,68 @@ using System.Collections;
 
 public class AutoFadeController : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 
 	}
 	// Update is called once per frame
 	void Update () {
-		if (LevelPlay.fading!=null ){
-			if (LevelPlay.fading.fadeIn == true) 
-				FadeIn();
-
-		}
-
 		if (LevelPlay.changeWorld == true) {
+			BackgroundManager.loadSkybox (LevelPlay.cam);
 			LevelPlay.changeWorld = false;
-			LevelPlay.changeWorldBackground (((int)((LevelPlay.gamePosition.y) / 2 + 1))-1);
 		}
+
+		if (LevelPlay.fading != null) {
+			if (LevelPlay.fading.fadeIn == true) {
+				LevelPlay.fading.fadeIn = false;
+				FadeIn ();
+			} else if (LevelPlay.fading.fadeOut == true) {
+				LevelPlay.fading.fadeOut = false;
+				FadeOut ();
+			} 
+			if (LevelPlay.fading.fadeInOut == true) {
+				LevelPlay.fading.fadeInOut = false;
+				FadeInOut ();
+			}
+		}
+		else if (Gameplay.fading != null) {
+			if (Gameplay.fading.fadeIn == true) {
+				Gameplay.fading.fadeIn = false;
+				FadeIn ();
+			} else if (Gameplay.fading.fadeOut == true) {
+				Gameplay.fading.fadeOut = false;
+				FadeOut ();
+			} else if (Gameplay.fading.fadeInOut == true) {
+				Gameplay.fading.fadeInOut = false;
+				FadeInOut ();
+			}
+		}
+
+
 	}
 
 	public void FadeIn(){
-		if (LevelPlay.fading.overlay == null) {
-			Debug.Log ("THERE IS NO OVERLAY GAMEOBJECT!");
-		}
-		else {
-			LevelPlay.fading.overlay.gameObject.SetActive (true);
-			LevelPlay.fading.overlay.pixelInset = new Rect (0, 0, Screen.width, Screen.height);
+		LevelPlay.fading.overlay.gameObject.SetActive (true);
+		LevelPlay.fading.overlay.pixelInset = new Rect (0, 0, Screen.width, Screen.height);
 
-			// fade to clear secondly
-			StartCoroutine(LevelPlay.fading.FadeIn());
+		StartCoroutine(LevelPlay.fading.FadeIn());
+	}
 
-		}
+	public void FadeOut(){
+		LevelPlay.fading.overlay.gameObject.SetActive (true);
+		LevelPlay.fading.overlay.pixelInset = new Rect (0, 0, Screen.width, Screen.height);
+
+		StartCoroutine(LevelPlay.fading.FadeOut());
+	}
+
+	public void FadeInOut(){
+		LevelPlay.fading.overlay.gameObject.SetActive (true);
+		LevelPlay.fading.overlay.pixelInset = new Rect (0, 0, Screen.width, Screen.height);
+
+		StartCoroutine(LevelPlay.fading.FadeInOut());
+	
+		LevelPlay.fading.fadeInOut = false;
+		InputManager.active=true;
 	}
 }

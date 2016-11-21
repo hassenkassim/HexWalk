9a8e2ddd8c4fa58d0b88924c0470c6ef42d4c029
Hassen Kassim <hass3n@googlemail.com>
2016-11-21 22:22:51 +0100

    * Progressbar in % * SplashLoad fixed * Skybox converted in pngs

--- Assets/Script/Manager/AdManager.cs
+++ Assets/Script/Manager/AdManager.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Advertisements;
+//using UnityEngine.Advertisements;
 
 public class AdManager : MonoBehaviour {
 
 	public static string adID ="videoAd";
 
 	public static void showAd(){
-		
-		if(Advertisement.IsReady ()){
+
+		//if(Advertisement.IsReady ()){
 			/*InputManager.active = false;
 			Advertisement.Show (adID, new ShowOptions(){resultCallback = handleAdResult});*/
-		}
+		//}
 
 
 	}
--- Assets/Script/Splash/SplashLoad.cs
+++ Assets/Script/Splash/SplashLoad.cs
@@ -11,8 +11,8 @@ public class SplashLoad : MonoBehaviour {
 	public static GameObject gameName;
 	public static string cubeName;
 
-	private Vector3 beginLight = new Vector3 (-10.0f, 10.0f, -1.0f);
-	private Vector3 endLight = new Vector3 (10.0f, 10.0f, -1.0f);
+	private Vector3 beginLight = new Vector3 (-5f, 10.0f, -1.0f);
+	private Vector3 endLight = new Vector3 (5f, 10.0f, -1.0f);
 
 	// Use this for initialization
 	void Start () {
@@ -36,7 +36,7 @@ public class SplashLoad : MonoBehaviour {
 		BackgroundManager.loadSkybox (cam);
 		BackgroundManager.setParticleSystem (cam);
 
-		setCubeName ("cube2");
+		setCubeName ("cube0");
 
 		//Load the Player
 		loadPlayer ();
@@ -77,7 +77,7 @@ public class SplashLoad : MonoBehaviour {
 	}
 		
 	private void moveLight(){
-		pointLight.transform.position = Vector3.Lerp (beginLight, endLight, Mathf.PingPong (Time.time*1.0f, 2.0f));
+		pointLight.transform.position = Vector3.Lerp (beginLight, endLight, Mathf.PingPong (Time.time*1.0f, 1f));
 	}
 
 	public static void setCubeName(string name){
